3b29ca7e50385da227c4867eb1da741a0c3a85bc
Jyothi888-JJ <jyothirmayi.jamisetti@gmail.com>
2026-09-19 11:03:41 +0530

    Remove EC2 instance outputs from outputs.tf
    Removed outputs for EC2 instance IDs and private IPs.

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
-output "instance_ids" {
-  description = "EC2 instance IDs"
-  value       = aws_instance.web[*].id
-}
-
-output "instance_private_ips" {
-  description = "Private IP addresses of EC2 instances"
-  value       = aws_instance.web[*].private_ip
-}
 
 output "load_balancer_dns" {
   description = "DNS name of the Application Load Balancer"
