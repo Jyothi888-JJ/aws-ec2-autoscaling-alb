59746d3da527cf5a6ab96a1c8941acfcd3c274cf
Jyothi888-JJ <jyothirmayi.jamisetti@gmail.com>
2026-09-19 10:59:22 +0530

    Create security group for ALB with HTTP ingress
    Added a new security group for ALB to allow HTTP traffic.

--- security-groups.tf
+++ security-groups.tf
@@ -26,6 +26,31 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+resource "aws_security_group" "alb" {
+  name        = "devops-alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-alb-sg"
+  }
+}
+
   tags = {
     Name = "devops-web-sg"
   }
