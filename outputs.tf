4d58cbfe8ce945ff311273202b092ee7d0b3e904
Jyothi888-JJ <jyothirmayi.jamisetti@gmail.com>
2026-09-19 10:59:53 +0530

    Add output for load balancer DNS name

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "instance_private_ips" {
   description = "Private IP addresses of EC2 instances"
   value       = aws_instance.web[*].private_ip
 }
+
+output "load_balancer_dns" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.web.dns_name
+}
