9416a3d24cc8d3e77c83bb15cb69b3adf6311e0e
Jyothi888-JJ <jyothirmayi.jamisetti@gmail.com>
2026-09-19 11:05:54 +0530

    Add outputs for autoscaling group and target group

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,13 @@ output "load_balancer_dns" {
   description = "DNS name of the Application Load Balancer"
   value       = aws_lb.web.dns_name
 }
+
+output "autoscaling_group_name" {
+  description = "Auto Scaling Group name"
+  value       = aws_autoscaling_group.web.name
+}
+
+output "target_group_arn" {
+  description = "Target Group ARN"
+  value       = aws_lb_target_group.web.arn
+}
